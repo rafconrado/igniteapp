03944d02dee2880cff5910bb910732939c83db37
Rafael Conrado <rafconradoo@gmail.com>
2025-04-08 17:38:56 -0300

    Header and Loading

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,23 @@
-import { Groups } from "@screens/groups";
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from '../src/theme';
+
+import { Groups } from '@screens/groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
